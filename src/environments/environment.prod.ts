6161d9030e62625593a477a83f394cd60ac32f90
Johnson <johnson266it@gmail.com>
2024-04-26 13:04:22 +0530

    commited to main

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  apiKey: "AIzaSyDGyAyR7ydScDTcv-KrYN6o9aqyTb-06pY",
+  authDomain: "dall-e-1de12.firebaseapp.com",
+  projectId: "dall-e-1de12",
+  storageBucket: "dall-e-1de12.appspot.com",
+  messagingSenderId: "11106236562",
+  appId: "1:11106236562:web:319d03635ce524af8f0e4b",
+  measurementId: "G-CT2Z5BGRPP",
+  dalle:"https://dall-e-server-otc3.onrender.com/api",
+  chat:"https://dall-e-server-otc3.onrender.com/chat"
+};
