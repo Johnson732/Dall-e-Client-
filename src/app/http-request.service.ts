9f38836e0a1b757912a1ec688a4bfb2c926cc4fa
Johnson <johnson266it@gmail.com>
2024-04-26 09:52:08 +0530

    dalle gpt integrated

--- src/app/http-request.service.ts
+++ src/app/http-request.service.ts
@@ -10,8 +10,19 @@ export class HttpRequestService {
 
   constructor(private Http:HttpClient) { }
   getData(prompt:string):any{
-    console.log("get data");
+    //console.log("get data");
     const url="http://localhost:3000/api";
+    //let params = { prompt: prompt };
+    return this.Http.post<any>(url,{prompt}).pipe(
+      catchError(error => {
+        console.error('Error occurred:', error);
+        return throwError('Something went wrong with the request.');
+      })
+    );
+  }
+  getChat(prompt:string):any{
+    //console.log("get chat");
+    const url="http://localhost:3000/chat";
     //let params = { prompt: prompt };
     return this.Http.post<any>(url,{prompt}).pipe(
       catchError(error => {
--- src/app/prompt/prompt.component.ts
+++ src/app/prompt/prompt.component.ts
@@ -4,22 +4,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-prompt',
   templateUrl: './prompt.component.html',
-  styleUrls: ['./prompt.component.css']
+  styleUrls: ['./prompt.component.css'],
 })
 export class PromptComponent {
-  prompt:string='';
-  imageSrc:string='';
-  constructor(private service : HttpRequestService){}
-  getUrl(){
-    console.log("get url");
-    this.service.getData(this.prompt).subscribe(
-      (response:any)=>{
-        console.log(response);
-        this.imageSrc=response.data[0].url;
-        console.log(this.imageSrc);
-      }
-    );
-
+  prompt: string = '';
+  imageSrc: string = '';
+  messages:string[]=[];
+  constructor(private service: HttpRequestService) {}
+  getUrl() {
+    //console.log('get url');
+    this.service.getData(this.prompt).subscribe((response: any) => {
+      //console.log(response);
+      this.imageSrc = response.data[0].url;
+      //console.log(this.imageSrc);
+    });
   }
 
+  getChats() {
+    //console.log('get url');
+    this.service.getChat(this.prompt).subscribe((response: any) => {
+      const messageString = response.choices[0].message.content;
+        const messagesArray = messageString.split("\n").map((message:any) => message.trim());
+        this.messages = messagesArray.filter((message:any) => message !== ''); // Remove empty strings
+
+      //console.log(this.messages);
+    });
+  }
+  selectMessage(selectedMessage: string) {
+    this.prompt = selectedMessage; // Set the prompt variable to the selected message
+    this.getUrl();
+  }
 }
